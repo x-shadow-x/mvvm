0343e0fd03fb9b7da26d6b1b309cbd23d8f38117
junyanLiu <2509344578@qq.com>
2018-03-31 18:52:50 +0800

    未添加虚拟dom

---
+++ compile.js
@@ -0,0 +1,207 @@
+function Compile(el, vm) {
+    this.$vm = vm;
+    this.$el = this.isElementNode(el) ? el : document.querySelector(el);
+
+    if (this.$el) {
+        this.$fragment = this.node2Fragment(this.$el);
+        this.init();
+        this.$el.appendChild(this.$fragment);
+    }
+}
+
+Compile.prototype = {
+    node2Fragment: function(el) {
+        var fragment = document.createDocumentFragment(),
+            child;
+
+        // 减少dom操作
+        while (child = el.firstChild) {
+            fragment.appendChild(child);
+        }
+
+        return fragment;
+    },
+
+    init: function() {
+        this.compileElement(this.$fragment);
+    },
+
+    compileElement: function(el) {
+        var childNodes = el.childNodes,
+            me = this;
+
+        [].slice.call(childNodes).forEach(function(node) {
+            var text = node.textContent;
+            var reg = /\{\{(.*)\}\}/;
+
+            if (me.isElementNode(node)) {
+                me.compile(node);
+
+            } else if (me.isTextNode(node) && reg.test(text)) {
+                me.compileText(node, RegExp.$1);
+            }
+
+            if (node.childNodes && node.childNodes.length) {
+                me.compileElement(node);
+            }
+        });
+    },
+
+    compile: function(node) {
+        var nodeAttrs = node.attributes,
+            me = this;
+
+        [].slice.call(nodeAttrs).forEach(function(attr) {
+            var attrName = attr.name;
+            if (me.isDirective(attrName)) {
+                var exp = attr.value;
+                var dir = attrName.substring(2);
+                // 事件指令
+                if (me.isEventDirective(dir)) {
+                    compileUtil.eventHandler(node, me.$vm, exp, dir);
+                    // 普通指令
+                } else {
+                    compileUtil[dir] && compileUtil[dir](node, me.$vm, exp);
+                }
+
+                node.removeAttribute(attrName);
+            }
+        });
+    },
+
+    compileText: function(node, exp) {
+        compileUtil.text(node, this.$vm, exp);
+    },
+
+    isDirective: function(attr) {
+        return attr.indexOf('v-') == 0;
+    },
+
+    isEventDirective: function(dir) {
+        return dir.indexOf('on') === 0;
+    },
+
+    isElementNode: function(node) {
+        return node.nodeType == 1;
+    },
+
+    isTextNode: function(node) {
+        return node.nodeType == 3;
+    }
+};
+
+var compileUtil = {
+
+    text: function(node, vm, exp) {
+        this.bind(node, vm, exp, 'text');
+    },
+
+    html: function(node, vm, exp) {
+        this.bind(node, vm, exp, 'html');
+    },
+
+    class: function(node, vm, exp) {
+        this.bind(node, vm, exp, 'class');
+    },
+
+    model: function(node, vm, exp) {
+        this.bind(node, vm, exp, 'model');
+
+        var me = this,
+            val = this._getVMVal(vm, exp);
+        node.addEventListener('input', function(e) {
+            var newValue = e.target.value;
+            if (val === newValue) {
+                return;
+            }
+
+            me._setVMVal(vm, exp, newValue);
+            val = newValue;
+        });
+    },
+
+    eventHandler: function(node, vm, exp, dir) {
+        console.log(node, vm, exp, dir);
+        var eventType = dir.split(':')[1],
+            fn = vm.$options.methods && vm.$options.methods[exp];
+
+        if (eventType && fn) {
+            node.addEventListener(eventType, fn.bind(vm), false);
+        }
+    },
+
+    bind: function(node, vm, exp, dir) {
+        // update对象中也根据4个指令定义了对应的更新函数~
+        var updaterFn = updater[dir + 'Updater'];
+
+        // 这里先调用一次用来初始化界面~比如html中写了这样一句：
+        // <p v-html="child.someStr"></p>
+        // 通过调用对应 htmlUpdater函数将指令中申明的属性对应的值初始化到p标签中
+        updaterFn && updaterFn(node, this._getVMVal(vm, exp));
+
+        new Watcher(vm, exp, function(value, oldValue) {
+            updaterFn && updaterFn(node, value, oldValue);
+        });
+    },
+
+
+    // 获取指令中对应的属性的值
+    // 例如：<input type="text" v-model="child.someStr">
+    // 则child.someStr一定是mvvm实例对象中data中的一个属性~其对应这一个值
+    // var vm = new MVVM({
+    //      el: '#mvvm-app',
+    //        data: {
+    //            someStr: 'hello ',
+    //            child: {
+    //                someStr: 'World !'
+    //            }
+    //        }
+    //    });
+    // 调用_getVMVal并传递exp为 'child.someStr'时~最后返回对应的值'World !'~
+    // 里面的for循环是为了在遇到多层嵌套的属性的时候可以获得正确的值
+    _getVMVal: function(vm, exp) {
+        var val = vm;
+        console.log(val);
+        exp = exp.split('.');
+        exp.forEach(function(k) {
+            val = val[k];
+        });
+        return val;
+    },
+
+    _setVMVal: function(vm, exp, value) {
+        var val = vm;
+        exp = exp.split('.');
+        exp.forEach(function(k, i) {
+            if (i < exp.length - 1) {
+                val = val[k];
+            } else {
+                val[k] = value;
+            }
+        });
+    }
+};
+
+
+var updater = {
+    textUpdater: function(node, value) {
+        node.textContent = typeof value == 'undefined' ? '' : value;
+    },
+
+    htmlUpdater: function(node, value) {
+        node.innerHTML = typeof value == 'undefined' ? '' : value;
+    },
+
+    classUpdater: function(node, value, oldValue) {
+        var className = node.className;
+        className = className.replace(oldValue, '').replace(/\s$/, '');
+
+        var space = className && String(value) ? ' ' : '';
+
+        node.className = className + space + value;
+    },
+
+    modelUpdater: function(node, value, oldValue) {
+        node.value = typeof value == 'undefined' ? '' : value;
+    }
+};
